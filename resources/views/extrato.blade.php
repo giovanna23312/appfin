5d34eb2e13c64d6251418835d9c250573bdb373f
giovanna23312 <111871714+giovanna23312@users.noreply.github.com>
2022-11-04 11:56:18 -0300

    Initial commit

---
+++ resources/views/extrato.blade.php
@@ -0,0 +1,31 @@
+@extends('template')
+
+
+@section('titulo', 'Extrato')
+
+@section('nav-complementar')
+<li class="nav-item">
+  <a class="nav-link" href="#">Entenda seus gastos</a>
+</li>
+
+@endsection 
+
+@section('conteudo')
+
+<header class="text-center">
+       EXTRATO - Saldo: R$ 9.999,00
+</header>
+
+<div class="row">
+
+<div class="col-md-6 p-3">
+    <h2>Receitas</h2>
+</div>
+
+<div class="col-md-6 p-3">
+    <h2>Despesas</h2>
+</div>
+
+</div>
+
+@endsection
